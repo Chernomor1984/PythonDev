e92bcc9ea6a12199077fc06e44848575a350d59a
Eugene Khizhnyak <chernomor1984@gmail.com>
2025-12-14 23:07:39 +0300

    Add lesson 7 task + rearrange dirs

---
+++ Homework/Lesson 7/1.py
@@ -0,0 +1,19 @@
+# Определяем функцию git_config_list, которая будет выполнять команду Git 
+# (нужно в консоль вывести результат работы команды git: git config --global --list)
+# Удаляем заглушку, создаем переменную result:
+# Используем subprocess.run для выполнения команды в переменной result
+# Выводим результат выполнения команды result.stdout
+
+import subprocess
+
+def git_config_list():
+    result = subprocess.run(
+        ["git", "config", "--global", "--list"],
+        capture_output=True,
+        text=True,
+        check=True
+    )
+    
+    print(result.stdout)
+    
+git_config_list()
